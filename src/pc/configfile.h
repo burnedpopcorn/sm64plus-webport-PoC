89c2827414628ac5088d910fa25d714175f91b88
burnedpopcorn180 <164114576+burnedpopcorn@users.noreply.github.com>
2024-05-11 22:32:41 -0700

    Delete src directory

--- src/pc/configfile.h
+++
@@ -1,7 +0,0 @@
-#ifndef CONFIGFILE_H
-#define CONFIGFILE_H
-
-void configfile_load(const char *filename);
-void configfile_save(const char *filename);
-
-#endif
--- src/pc/gfx/gfx_screen_config.h
+++
@@ -1,7 +0,0 @@
-#ifndef GFX_SCREEN_CONFIG_H
-#define GFX_SCREEN_CONFIG_H
-
-#define DESIRED_SCREEN_WIDTH configWindowWidth
-#define DESIRED_SCREEN_HEIGHT configWindowHeight
-
-#endif
